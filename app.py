518c711dc7dc8eb1ac761bba359cf1e3638fba0d
saurabh kumar <saurabh742@gmail.com>
2019-06-01 12:37:41 +0530

    added slack client code

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask,request
+import slack
 
 app = Flask(__name__)
 
 @app.route('/',methods = ['GET','POST'])
 def index():
     req_data = request.get_json()
-    return(req_data.get('challenge'))
+    client = slack.WebClient(token='xoxp-614042023619-627665357718-619191220513-e5cd817b97126dc3ab32b90b88e66b2f')
+    response = client.conversations_history(channel='CJDEY1Q5N',limit=1)
+    assert response["ok"]
+     
+    for resp in response["messages"]:
+        if resp['type'] == "message":
+            print(resp['text'])
+						
+    return(resp['text'])
     
 
 		
